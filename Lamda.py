1b4c230c9356b7df9c01874ebe59c9dc864e4a8c
Rifat Al Shajid <64960131+rifatalshajid@users.noreply.github.com>
2025-02-11 10:57:27 +0600

    Create Lamda.py

---
+++ Lamda.py
@@ -0,0 +1,18 @@
+# Python Lamda ====================== namhin functions kei lamda bole.
+
+
+# Define function
+
+def myFun(a, b):
+    sum = a + b
+
+    print(sum)
+
+myFun(10, 20)
+
+
+# undefine function
+
+x = lambda a,b: a + b
+
+print(x(20, 30))
