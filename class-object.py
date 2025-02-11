539a8774f8c93d86a4a61778d33fc46b26c26a54
Rifat Al Shajid <64960131+rifatalshajid@users.noreply.github.com>
2025-02-11 10:52:30 +0600

    Add files via upload

---
+++ class-object.py
@@ -0,0 +1,18 @@
+# Clss amd object
+
+class employee:
+
+    name = ""
+    number = ""
+
+a = employee()
+b = employee()
+
+a.name = "Rifat Al Shajid"
+a.number = 1325412555
+
+b.name = "Ashrafujjaman Tutul"
+b.number = 1241005454
+
+print(b.number)
+print(a.name)
