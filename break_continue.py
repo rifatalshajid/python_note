aa7045db10607b4f5151e07a471d958a1e8c73ec
Rifat Al Shajid <64960131+rifatalshajid@users.noreply.github.com>
2025-02-11 10:56:28 +0600

    Update break_continue.py

--- break_continue.py
+++ break_continue.py
@@ -36,25 +36,6 @@
 #     i = i + 2
 
 
-# Python Lamda ====================== namhin functions kei lamda bole.
-
-
-# Define function
-
-# def myFun(a, b):
-#     sum = a + b
-#
-#     print(sum)
-#
-# myFun(10, 20)
-
-
-# undefine function
-
-# x = lambda a,b: a + b
-#
-# print(x(20, 30))
-
 # Python array =====================
 
 rifat = ["Rifat", "Al", "Shajid"]
